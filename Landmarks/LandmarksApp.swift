9e7530e59f37840a65e46dc66bd107c4adae9bac
Chandler Cheng <dc9909@hotmail.com>
2021-06-14 13:08:56 +1200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chandler Cheng on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
