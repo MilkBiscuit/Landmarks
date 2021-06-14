eb4e925d1339d98f00af68d1f4effe3b28305f7e
Chandler Cheng <dc9909@hotmail.com>
2021-06-15 09:13:32 +1200

    Finish section 5 of 'Building Lists and Navs'

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Chandler Cheng on 15/06/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
