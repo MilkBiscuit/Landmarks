095f57c24042f74fdf88f296b9179ae2e624b62d
Chandler Cheng <dc9909@hotmail.com>
2021-06-17 11:47:25 +1200

    Finish section 1 of 'Working with UI Controls'

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Chandler Cheng on 16/06/21.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
