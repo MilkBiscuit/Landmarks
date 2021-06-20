1d964e5ccdd7715be2b833aef1493f24d152d46d
Chandler Cheng <dc9909@hotmail.com>
2021-06-21 09:04:48 +1200

    Finish section 2 of 'Creating a watchOS App'

---
+++ WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Chandler Cheng on 21/06/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
